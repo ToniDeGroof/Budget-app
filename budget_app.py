61f965d9b0f71d7d0b7136ba27d1b366a02d54db
ToniDeGroof <toni.degroof@telenet.be>
2025-04-24 14:41:48 +0200

    Update budget_app.py

--- budget_app.py
+++ budget_app.py
@@ -32,6 +32,29 @@ if submitted:
     else:
         volledige_data = nieuwe_data
 
+    # Google Sheets-authenticatie
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+client = gspread.authorize(credentials)
+
+# Verbind met je Google Spreadsheet (pas naam aan indien nodig)
+sheet = client.open("DeAfrekening").sheet1
+
+# Formulier
+with st.form("afrekening_formulier"):
+    datum = st.date_input("Datum", format="DD/MM/YYYY")
+    winkel = st.text_input("Winkel")
+    persoon = st.selectbox("Persoon", ["Ik", "Partner"])
+    bedrag = st.number_input("Bedrag", min_value=0.0, step=0.01, format="%.2f")
+    verzenden = st.form_submit_button("Toevoegen")
+
+    if verzenden:
+        datum_str = datum.strftime("%d/%m/%Y")
+        nieuwe_gegevens = [datum_str, winkel, persoon, str(bedrag)]
+        sheet.append_row(nieuwe_gegevens)
+        st.success("Gegevens toegevoegd aan Google Sheet!")
+
+
     # Opslaan
     volledige_data.to_excel(bestand, index=False)
 
