f5546a142eddacf45f3d0be1e0fabcb59d68e9e1
ToniDeGroof <toni.degroof@telenet.be>
2025-04-24 10:07:58 +0200

    Update budget_app.py

--- budget_app.py
+++ budget_app.py
@@ -1,38 +1,38 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from datetime import date
+from openpyxl import load_workbook
 import os
 
-# Pad naar Excel-bestand (in dezelfde map als dit script)
-excel_pad = "budget_data.xlsx"
+# Titel en layout
+st.markdown("<h1 style='text-align: center;'>De Afrekening</h1>", unsafe_allow_html=True)
+st.markdown("### Geef je uitgave in:")
 
-# Als bestand niet bestaat, maak een lege tabel aan
-if not os.path.exists(excel_pad):
-    df_init = pd.DataFrame(columns=["Datum", "Winkel", "Persoon", "Bedrag"])
-    df_init.to_excel(excel_pad, index=False)
-
-st.title("De Afrekening")
-
-# Formulier tonen
-with st.form("boodschappen_formulier"):
-    datum = st.date_input("Datum", value=datetime.today())
+# Formulier met nette indeling
+with st.form("expense_form"):
+    datum = st.date_input("Datum", value=date.today())
     winkel = st.text_input("Winkel")
-    persoon = st.text_input("Persoon")
-    bedrag = st.number_input("Bedrag", format="%.2f", step=0.01)
+    persoon = st.selectbox("Persoon", ["Jan", "Els"])
+    bedrag = st.number_input("Bedrag (€)", min_value=0.01, format="%.2f")
+    
+    submitted = st.form_submit_button("Toevoegen")
 
-    verzenden = st.form_submit_button("Toevoegen aan Excel")
+if submitted:
+    # Pad naar Excel-bestand (in dezelfde map als script)
+    bestand = "uitgaven.xlsx"
 
-    if verzenden:
-        nieuwe_data = pd.DataFrame({
-            "Datum": [datum.strftime("%d-%m-%Y")],
-            "Winkel": [winkel],
-            "Persoon": [persoon],
-            "Bedrag": [bedrag]
-        })
+    # Nieuwe rij met gegevens
+    nieuwe_data = pd.DataFrame([[datum, winkel, persoon, bedrag]],
+                               columns=["Datum", "Winkel", "Persoon", "Bedrag"])
 
-        # Gegevens opslaan
-        bestaand = pd.read_excel(excel_pad)
-        totaal = pd.concat([bestaand, nieuwe_data], ignore_index=True)
-        totaal.to_excel(excel_pad, index=False)
+    # Bestaat het bestand al?
+    if os.path.exists(bestand):
+        bestaande_data = pd.read_excel(bestand)
+        volledige_data = pd.concat([bestaande_data, nieuwe_data], ignore_index=True)
+    else:
+        volledige_data = nieuwe_data
 
-        st.success("Gegevens toegevoegd aan Excel!")
+    # Opslaan
+    volledige_data.to_excel(bestand, index=False)
+
+    st.success("Uitgave toegevoegd!")
